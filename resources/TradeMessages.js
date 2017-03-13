366aa542d87ae6bd9d61efa4c7778671838d5ed8
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2017-03-13 17:20:14 +0200

    Change `InvalidApp` message

--- resources/TradeMessages.js
+++ resources/TradeMessages.js
@@ -6,7 +6,7 @@ module.exports = {
         Overpay: 'Sorry! Your trade was rejected, because you did not overpay enough.',
         ItemMissing: 'Sorry! Your trade was rejected, because you did not add any items on your side.',
         Escrow: 'Sorry! Your trade was rejected, because you do not have two-factor authentication enabled. We do not accept trades that go into escrow.',
-        InvalidApp: 'Sorry! Your trade was rejected, because we do not accept items for appID: ',
+        InvalidApp: 'Sorry! Your trade was rejected, because we do not accept items from one or more appID\'s you selected.',
         Unknown: 'Sorry! Your trade was rejected, because something went wrong with verifying the trade. Please try again later or contact the owner!',
     },
     Success: {
